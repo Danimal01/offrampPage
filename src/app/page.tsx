6614ccb09a861f516dca494ff744e3583f45cd4b
Danimal01 <danialdadkhoo@gmail.com>
2024-07-03 10:41:13 -0400

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-// pages/index.tsx (or page.tsx if that's the file name)
+'use client';
 import styles from './page.module.css';
 
 export default function Home() {
+  const handleButtonClick = () => {
+    const url = "https://offramp.gatefi.com/?merchantId=77f72e08-b9a5-47f9-9cbb-99856c8fffde&cryptoCurrency=USDC_SOL&payout=SPEI&fiatCurrency=MXN&region=MX&confirmRedirectUrl=https://offramp-page.vercel.app/confirm/";
+    window.open(url, '_blank');
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.iframeContainer}>
-        <iframe
-          src="https://offramp.gatefi.com/?merchantId=77f72e08-b9a5-47f9-9cbb-99856c8fffde&cryptoCurrency=USDC_SOL&payout=SPEI&fiatCurrency=MXN&region=MX&confirmRedirectUrl=https://offramp-page.vercel.app/confirm/"
-          className={styles.iframe}
-          allowFullScreen
-        ></iframe>
+      <div className={styles.buttonContainer}>
+        <button onClick={handleButtonClick} className={styles.sellButton}>
+          Sell Crypto
+        </button>
       </div>
     </main>
   );
